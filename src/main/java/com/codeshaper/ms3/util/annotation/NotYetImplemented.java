9b1f05027ae6c4bbc82963dcb1697b348331920d
pjdidelot <19332219+CodeShaper13@users.noreply.github.com>
2019-09-27 09:58:11 -0400

    Moved the locaiton of the NotYetImplemented source file.

--- src/main/java/com/codeshaper/ms3/util/annotation/NotYetImplemented.java
+++ src/main/java/com/codeshaper/ms3/util/annotation/NotYetImplemented.java
@@ -1,4 +1,4 @@
-package com.codeshaper.ms3.annotation;
+package com.codeshaper.ms3.util.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,11 +9,13 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * Used to mark an annotation, method, constructor, field or class as not yet
- * fully implemented.
+ * implemented completely. The marked element may be partially implemented or
+ * just an empty class.
  * 
  * @author CodeShaper
  */
-@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.TYPE })
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.FIELD,
+		ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotYetImplemented {
 	String notes() default StringUtils.EMPTY;
