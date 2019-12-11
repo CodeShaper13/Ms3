faee4a456464066b6c1499052ee928e29c9a2c54
pjdidelot <19332219+CodeShaper13@users.noreply.github.com>
2019-12-11 08:14:53 -0500

    Changed biome names from hard coded constants to getting the name from the vanilla registry.

--- src/main/java/com/codeshaper/ms3/api/biomes.java
+++ src/main/java/com/codeshaper/ms3/api/biomes.java
@@ -3,152 +3,172 @@ package com.codeshaper.ms3.api;
 import org.python.core.PyString;
 import org.python.core.PyList;
 
+import com.codeshaper.ms3.api.tileEntity.Beacon;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonClass;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonDocString;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonFieldGenerated;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonFunction;
 
+import net.minecraft.init.Biomes;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 @PythonClass
 @PythonDocString("A list of all the vanilla Biome names.")
 public class biomes {
 
-    private static PyList names = new PyList();
-
     private biomes() { }
 
     @PythonFieldGenerated
-    public static final String OCEAN = func("minecraft:ocean");
+    public static final String OCEAN = getRegName(Biomes.OCEAN);
     @PythonFieldGenerated
-    public static final String PLAINS = func("minecraft:plains");
+    public static final String PLAINS = getRegName(Biomes.PLAINS);
     @PythonFieldGenerated
-    public static final String DESERT = func("minecraft:desert");
+    public static final String DESERT = getRegName(Biomes.DESERT);
     @PythonFieldGenerated
-    public static final String EXTREME_HILLS = func("minecraft:extreme_hills");
+    public static final String EXTREME_HILLS = getRegName(Biomes.EXTREME_HILLS);
     @PythonFieldGenerated
-    public static final String FOREST = func("minecraft:forest");
+    public static final String FOREST = getRegName(Biomes.FOREST);
     @PythonFieldGenerated
-    public static final String TAIGA = func("minecraft:taiga");
+    public static final String TAIGA = getRegName(Biomes.TAIGA);
     @PythonFieldGenerated
-    public static final String SWAMPLAND = func("minecraft:swampland");
+    public static final String SWAMPLAND = getRegName(Biomes.SWAMPLAND);
     @PythonFieldGenerated
-    public static final String RIVER = func("minecraft:river");
+    public static final String RIVER = getRegName(Biomes.RIVER);
     @PythonFieldGenerated
-    public static final String HELL = func("minecraft:hell");
+    public static final String HELL = getRegName(Biomes.HELL);
     @PythonFieldGenerated
-    public static final String SKY = func("minecraft:sky");
+    public static final String SKY = getRegName(Biomes.SKY);
     @PythonFieldGenerated
-    public static final String FROZEN_OCEAN = func("minecraft:frozen_ocean");
+    public static final String FROZEN_OCEAN = getRegName(Biomes.FROZEN_OCEAN);
     @PythonFieldGenerated
-    public static final String FROZEN_RIVER = func("minecraft:frozen_river");
+    public static final String FROZEN_RIVER = getRegName(Biomes.FROZEN_RIVER);
     @PythonFieldGenerated
-    public static final String ICE_FLATS = func("minecraft:ice_flats");
+    public static final String ICE_FLATS = getRegName(Biomes.MUTATED_ICE_FLATS);
     @PythonFieldGenerated
-    public static final String ICE_MOUNTAINS = func("minecraft:ice_mountains");
+    public static final String ICE_MOUNTAINS = getRegName(Biomes.ICE_MOUNTAINS);
     @PythonFieldGenerated
-    public static final String MUSHROOM_ISLAND = func("minecraft:mushroom_island");
+    public static final String MUSHROOM_ISLAND = getRegName(Biomes.MUSHROOM_ISLAND);
     @PythonFieldGenerated
-    public static final String MUSHROOM_ISLAND_SHORE = func("minecraft:mushroom_island_shore");
+    public static final String MUSHROOM_ISLAND_SHORE = getRegName(Biomes.MUSHROOM_ISLAND_SHORE);
     @PythonFieldGenerated
-    public static final String BEACHES = func("minecraft:beaches");
+    public static final String BEACHE = getRegName(Biomes.BEACH);
     @PythonFieldGenerated
-    public static final String DESERT_HILLS = func("minecraft:desert_hills");
+    public static final String DESERT_HILLS = getRegName(Biomes.DESERT_HILLS);
     @PythonFieldGenerated
-    public static final String FOREST_HILLS = func("minecraft:forest_hills");
+    public static final String FOREST_HILLS = getRegName(Biomes.FOREST_HILLS);
     @PythonFieldGenerated
-    public static final String TAIGA_HILLS = func("minecraft:taiga_hills");
+    public static final String TAIGA_HILLS = getRegName(Biomes.TAIGA_HILLS);
     @PythonFieldGenerated
-    public static final String SMALLER_EXTREME_HILLS = func("minecraft:smaller_extreme_hills");
+    public static final String SMALLER_EXTREME_HILLS = getRegName(Biomes.EXTREME_HILLS_EDGE);
     @PythonFieldGenerated
-    public static final String JUNGLE = func("minecraft:jungle");
+    public static final String JUNGLE = getRegName(Biomes.JUNGLE);
     @PythonFieldGenerated
-    public static final String JUNGLE_HILLS = func("minecraft:jungle_hills");
+    public static final String JUNGLE_HILLS = getRegName(Biomes.JUNGLE_HILLS);
     @PythonFieldGenerated
-    public static final String JUNGLE_EDGE = func("minecraft:jungle_edge");
+    public static final String JUNGLE_EDGE = getRegName(Biomes.JUNGLE_HILLS);
     @PythonFieldGenerated
-    public static final String DEEP_OCEAN = func("minecraft:deep_ocean");
+    public static final String DEEP_OCEAN = getRegName(Biomes.DEEP_OCEAN);
     @PythonFieldGenerated
-    public static final String STONE_BEACH = func("minecraft:stone_beach");
+    public static final String STONE_BEACH = getRegName(Biomes.STONE_BEACH);
     @PythonFieldGenerated
-    public static final String COLD_BEACH = func("minecraft:cold_beach");
+    public static final String COLD_BEACH = getRegName(Biomes.COLD_BEACH);
     @PythonFieldGenerated
-    public static final String BIRCH_FOREST = func("minecraft:birch_forest");
+    public static final String BIRCH_FOREST = getRegName(Biomes.BIRCH_FOREST);
     @PythonFieldGenerated
-    public static final String BIRCH_FOREST_HILLS = func("minecraft:birch_forest_hills");
+    public static final String BIRCH_FOREST_HILLS = getRegName(Biomes.BIRCH_FOREST_HILLS);
     @PythonFieldGenerated
-    public static final String ROOFED_FOREST = func("minecraft:roofed_forest");
+    public static final String ROOFED_FOREST = getRegName(Biomes.ROOFED_FOREST);
     @PythonFieldGenerated
-    public static final String TAIGA_COLD = func("minecraft:taiga_cold");
+    public static final String TAIGA_COLD = getRegName(Biomes.COLD_TAIGA);
     @PythonFieldGenerated
-    public static final String TAIGA_COLD_HILLS = func("minecraft:taiga_cold_hills");
+    public static final String TAIGA_COLD_HILLS = getRegName(Biomes.COLD_TAIGA_HILLS);
     @PythonFieldGenerated
-    public static final String REDWOOD_TAIGA = func("minecraft:redwood_taiga");
+    public static final String REDWOOD_TAIGA = getRegName(Biomes.REDWOOD_TAIGA);
     @PythonFieldGenerated
-    public static final String REDWOOD_TAIGA_HILLS = func("minecraft:redwood_taiga_hills");
+    public static final String REDWOOD_TAIGA_HILLS = getRegName(Biomes.REDWOOD_TAIGA_HILLS);
     @PythonFieldGenerated
-    public static final String EXTREME_HILLS_WITH_TREES = func("minecraft:extreme_hills_with_trees");
+    public static final String EXTREME_HILLS_WITH_TREES = getRegName(Biomes.EXTREME_HILLS_WITH_TREES);
     @PythonFieldGenerated
-    public static final String SAVANNA = func("minecraft:savanna");
+    public static final String SAVANNA = getRegName(Biomes.SAVANNA);
     @PythonFieldGenerated
-    public static final String SAVANNA_ROCK = func("minecraft:savanna_rock");
+    public static final String SAVANNA_PLATEAU = getRegName(Biomes.SAVANNA_PLATEAU);
     @PythonFieldGenerated
-    public static final String MESA = func("minecraft:mesa");
+    public static final String MESA = getRegName(Biomes.MESA);
     @PythonFieldGenerated
-    public static final String MESA_ROCK = func("minecraft:mesa_rock");
+    public static final String MESA_ROCK = getRegName(Biomes.MUTATED_MESA_ROCK);
     @PythonFieldGenerated
-    public static final String MESA_CLEAR_ROCK = func("minecraft:mesa_clear_rock");
+    public static final String MESA_CLEAR_ROCK = getRegName(Biomes.MUTATED_MESA_CLEAR_ROCK);
     @PythonFieldGenerated
-    public static final String VOID = func("minecraft:void");
+    public static final String VOID = getRegName(Biomes.VOID);
     @PythonFieldGenerated
-    public static final String MUTATED_PLAINS = func("minecraft:mutated_plains");
+    public static final String MUTATED_PLAINS = getRegName(Biomes.MUTATED_PLAINS);
     @PythonFieldGenerated
-    public static final String MUTATED_DESERT = func("minecraft:mutated_desert");
+    public static final String MUTATED_DESERT = getRegName(Biomes.MUTATED_DESERT);
     @PythonFieldGenerated
-    public static final String MUTATED_EXTREME_HILLS = func("minecraft:mutated_extreme_hills");
+    public static final String MUTATED_EXTREME_HILLS = getRegName(Biomes.MUTATED_EXTREME_HILLS);
     @PythonFieldGenerated
-    public static final String MUTATED_FOREST = func("minecraft:mutated_forest");
+    public static final String MUTATED_FOREST = getRegName(Biomes.MUTATED_FOREST);
     @PythonFieldGenerated
-    public static final String MUTATED_TAIGA = func("minecraft:mutated_taiga");
+    public static final String MUTATED_TAIGA = getRegName(Biomes.MUTATED_TAIGA);
     @PythonFieldGenerated
-    public static final String MUTATED_SWAMPLAND = func("minecraft:mutated_swampland");
+    public static final String MUTATED_SWAMPLAND = getRegName(Biomes.MUTATED_SWAMPLAND);
     @PythonFieldGenerated
-    public static final String MUTATED_ICE_FLATS = func("minecraft:mutated_ice_flats");
+    public static final String MUTATED_ICE_FLATS = getRegName(Biomes.MUTATED_ICE_FLATS);
     @PythonFieldGenerated
-    public static final String MUTATED_JUNGLE = func("minecraft:mutated_jungle");
+    public static final String MUTATED_JUNGLE = getRegName(Biomes.MUTATED_JUNGLE);
     @PythonFieldGenerated
-    public static final String MUTATED_JUNGLE_EDGE = func("minecraft:mutated_jungle_edge");
+    public static final String MUTATED_JUNGLE_EDGE = getRegName(Biomes.MUTATED_JUNGLE_EDGE);
     @PythonFieldGenerated
-    public static final String MUTATED_BIRCH_FOREST = func("minecraft:mutated_birch_forest");
+    public static final String MUTATED_BIRCH_FOREST = getRegName(Biomes.MUTATED_BIRCH_FOREST);
     @PythonFieldGenerated
-    public static final String MUTATED_BIRCH_FOREST_HILLS = func("minecraft:mutated_birch_forest_hills");
+    public static final String MUTATED_BIRCH_FOREST_HILLS = getRegName(Biomes.MUTATED_BIRCH_FOREST_HILLS);
     @PythonFieldGenerated
-    public static final String MUTATED_ROOFED_FOREST = func("minecraft:mutated_roofed_forest");
+    public static final String MUTATED_ROOFED_FOREST = getRegName(Biomes.MUTATED_ROOFED_FOREST);
     @PythonFieldGenerated
-    public static final String MUTATED_TAIGA_COLD = func("minecraft:mutated_taiga_cold");
+    public static final String MUTATED_TAIGA_COLD = getRegName(Biomes.MUTATED_TAIGA_COLD);
     @PythonFieldGenerated
-    public static final String MUTATED_REDWOOD_TAIGA = func("minecraft:mutated_redwood_taiga");
+    public static final String MUTATED_REDWOOD_TAIGA = getRegName(Biomes.MUTATED_REDWOOD_TAIGA);
     @PythonFieldGenerated
-    public static final String MUTATED_REDWOOD_TAIGA_HILLS = func("minecraft:mutated_redwood_taiga_hills");
+    public static final String MUTATED_REDWOOD_TAIGA_HILLS = getRegName(Biomes.MUTATED_REDWOOD_TAIGA_HILLS);
     @PythonFieldGenerated
-    public static final String MUTATED_EXTREME_HILLS_WITH_TREES = func("minecraft:mutated_extreme_hills_with_trees");
+    public static final String MUTATED_EXTREME_HILLS_WITH_TREES = getRegName(Biomes.MUTATED_EXTREME_HILLS_WITH_TREES);
     @PythonFieldGenerated
-    public static final String MUTATED_SAVANNA = func("minecraft:mutated_savanna");
+    public static final String MUTATED_SAVANNA = getRegName(Biomes.MUTATED_SAVANNA);
     @PythonFieldGenerated
-    public static final String MUTATED_SAVANNA_ROCK = func("minecraft:mutated_savanna_rock");
+    public static final String MUTATED_SAVANNA_ROCK = getRegName(Biomes.MUTATED_SAVANNA_ROCK);
     @PythonFieldGenerated
-    public static final String MUTATED_MESA = func("minecraft:mutated_mesa");
+    public static final String MUTATED_MESA = getRegName(Biomes.MUTATED_MESA);
     @PythonFieldGenerated
-    public static final String MUTATED_MESA_ROCK = func("minecraft:mutated_mesa_rock");
+    public static final String MUTATED_MESA_ROCK = getRegName(Biomes.MUTATED_MESA_ROCK);
     @PythonFieldGenerated
-    public static final String MUTATED_MESA_CLEAR_ROCK = func("minecraft:mutated_mesa_clear_rock");
+    public static final String MUTATED_MESA_CLEAR_ROCK = getRegName(Biomes.MUTATED_MESA_CLEAR_ROCK);
 
     @PythonFunction
-    @PythonDocString("Returns a list of all the vanilla Biomes.")
+    @PythonDocString("Returns a list of all the Biomes.  This will include mod biomes that were registered correctly.")
     public static PyList getAllBiomes() {
-        return names;
+        return func02(true);
+    }
+    
+    @PythonFunction
+    @PythonDocString("Returns a list of all the vanilla Biomes.")
+    public static PyList getAllVanillaBiomes() {
+        return func02(false);
+    }
+    
+    private static PyList func02(boolean includeModBiomes) {
+    	PyList list = new PyList();
+    	for(ResourceLocation rl : Biome.REGISTRY.getKeys()) {    		
+    		if(includeModBiomes || rl.getResourceDomain().equals("minecraft")) {
+    			list.add(rl.toString());
+    		}
+    	}
+    	return list;
     }
 
-    private static String func(String s) {
-        names.add(new PyString(s));
-        return s;
+    private static String getRegName(Biome biome) {
+    	return biome.getRegistryName().toString();
     }
 }
