e093b81807552ded687e868f8449a288fc0e19f2
pjdidelot <19332219+CodeShaper13@users.noreply.github.com>
2019-12-11 08:12:53 -0500

    Split color ids from tuples to seperated fields.

--- src/main/java/com/codeshaper/ms3/api/colors.java
+++ src/main/java/com/codeshaper/ms3/api/colors.java
@@ -5,52 +5,86 @@ import org.python.core.PyTuple;
 
 import com.codeshaper.ms3.apiBuilder.annotation.PythonClass;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonDocString;
+import com.codeshaper.ms3.apiBuilder.annotation.PythonField;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonFieldGenerated;
 import com.codeshaper.ms3.apiBuilder.annotation.PythonFunction;
 import com.codeshaper.ms3.util.Util;
 
 @PythonClass
-@PythonDocString("Constants for the different colors.  First item is ID and the second is it's dye ID.")
+@PythonDocString("Constants for the different colors in Minecraft.  The 16 colors in Minecraft are mapped to numeric ids in two different ways, depending on the situation.  Colored blocks, nbt tags dealing with color and most situations have the first color, white, mapped to 0, and the last color black, as 15.  Dye items meta dat is reversed, with black having a meta of 0, and white having a meta of 15.")
 public class colors {
 
 	private colors() {
 	}
 
-	@PythonFieldGenerated
-	public static final PyTuple WHITE = Util.makeTuple(0, 15);
-	@PythonFieldGenerated
-	public static final PyTuple ORANGE = Util.makeTuple(1, 14);
-	@PythonFieldGenerated
-	public static final PyTuple MAGENTA = Util.makeTuple(2, 13);
-	@PythonFieldGenerated
-	public static final PyTuple LIGHT_BLUE = Util.makeTuple(3, 12);
-	@PythonFieldGenerated
-	public static final PyTuple YELLOW = Util.makeTuple(4, 11);
-	@PythonFieldGenerated
-	public static final PyTuple LIME = Util.makeTuple(5, 10);
-	@PythonFieldGenerated
-	public static final PyTuple PINK = Util.makeTuple(6, 9);
-	@PythonFieldGenerated
-	public static final PyTuple GRAY = Util.makeTuple(7, 8);
-	@PythonFieldGenerated
-	public static final PyTuple LIGHT_GRAY = Util.makeTuple(8, 7);
-	@PythonFieldGenerated
-	public static final PyTuple CYAN = Util.makeTuple(9, 6);
-	@PythonFieldGenerated
-	public static final PyTuple PURPLE = Util.makeTuple(10, 5);
-	@PythonFieldGenerated
-	public static final PyTuple BLUE = Util.makeTuple(11, 4);
-	@PythonFieldGenerated
-	public static final PyTuple BROWN = Util.makeTuple(12, 3);
-	@PythonFieldGenerated
-	public static final PyTuple GREEN = Util.makeTuple(13, 2);
-	@PythonFieldGenerated
-	public static final PyTuple RED = Util.makeTuple(14, 1);
-	@PythonFieldGenerated
-	public static final PyTuple BLACK = Util.makeTuple(15, 0);
+	@PythonField
+	public static final int WHITE = 0;
+	@PythonField
+	public static final int ORANGE = 1;
+	@PythonField
+	public static final int MAGENTA = 2;
+	@PythonField
+	public static final int LIGHT_BLUE = 3;
+	@PythonField
+	public static final int YELLOW = 4;
+	@PythonField
+	public static final int LIME = 5;
+	@PythonField
+	public static final int PINK = 6;
+	@PythonField
+	public static final int GRAY = 7;
+	@PythonField
+	public static final int LIGHT_GRAY = 8;
+	@PythonField
+	public static final int CYAN = 9;
+	@PythonField
+	public static final int PURPLE = 10;
+	@PythonField
+	public static final int BLUE = 11;
+	@PythonField
+	public static final int BROWN = 12;
+	@PythonField
+	public static final int GREEN = 13;
+	@PythonField
+	public static final int RED = 14;
+	@PythonField
+	public static final int BLACK = 15;
+	
+	@PythonField
+	public static final int DYE_WHITE = 15;
+	@PythonField
+	public static final int DYE_ORANGE = 14;
+	@PythonField
+	public static final int DYE_MAGENTA = 13;
+	@PythonField
+	public static final int DYE_LIGHT_BLUE = 12;
+	@PythonField
+	public static final int DYE_YELLOW = 11;
+	@PythonField
+	public static final int DYE_LIME = 10;
+	@PythonField
+	public static final int DYE_PINK = 9;
+	@PythonField
+	public static final int DYE_GRAY = 8;
+	@PythonField
+	public static final int DYE_LIGHT_GRAY = 7;
+	@PythonField
+	public static final int DYE_CYAN = 6;
+	@PythonField
+	public static final int DYE_PURPLE = 5;
+	@PythonField
+	public static final int DYE_BLUE = 4;
+	@PythonField
+	public static final int DYE_BROWN = 3;
+	@PythonField
+	public static final int DYE_GREEN = 2;
+	@PythonField
+	public static final int DYE_RED = 1;
+	@PythonField
+	public static final int DYE_BLACK = 0;
 
 	@PythonFunction
-	@PythonDocString("Returns a list of all colors, ordered by their ID.")
+	@PythonDocString("Returns a list of all the color, ordered by their ID.")
 	public static PyList getAllColors() {
 		PyList list = new PyList();
 		list.add(WHITE);
